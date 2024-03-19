a5291ff79caa9031b9ab9fd258c358821a69efd1
Steven-Awo <rotimiawomodu@gmail.com>
2024-03-19 20:18:00 +0100

    env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  # Updated to match the URL path for static files
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'  # Updated to match the URL path for media files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
